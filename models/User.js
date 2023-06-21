7858d4968fbacedc03d009516275741c8c7cd226
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-06-21 19:51:21 +0100

    Jwt setup(create and verify) done

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -19,8 +20,13 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
-    maxlength: 12,
   },
+})
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 module.exports = mongoose.model("User", UserSchema)
