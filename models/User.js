6a481b675ca197438380884537f61ec09f44ac12
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-06-21 12:56:07 +0100

    User model added

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide name"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+    maxlength: 12,
+  },
+})
+
+module.exports = mongoose.model("User", UserSchema)
