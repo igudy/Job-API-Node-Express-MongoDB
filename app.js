008eace5602ece0e4909aef42a4b815ddbe52b2c
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-06-23 18:30:05 +0100

    Bug fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require("express-async-errors")
 const helmet = require("helmet")
 const cors = require("cors")
 const xss = require("xss-clean")
-const rateLimit = require("express-rate-limit")
+const rateLimiter = require("express-rate-limit")
 
 const express = require("express")
 const app = express()
@@ -29,7 +29,7 @@ app.use(
 )
 
 app.use(express.json())
-app.use(hemlet())
+app.use(helmet())
 app.use(cors())
 app.use(xss())
 
